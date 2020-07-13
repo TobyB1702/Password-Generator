ba3706d6e4475a2cb750b9c1c814a7e25beb8073
toby <toby.bell1702@hotmail.co.uk>
2020-07-13 11:52:11 +0100

    Added random string of a size of 12

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ letters = string.ascii_letters
 symbols = string.punctuation
 randomword = random.choice(letters)
 
-print(symbols)
+random = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation ) for n in range(12)])
+print(random)
