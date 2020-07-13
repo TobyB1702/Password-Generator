70f0c04f7dcb2a0185d8e1beb4119377d178da97
toby <toby.bell1702@hotmail.co.uk>
2020-07-13 11:58:50 +0100

    moved into method

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import random
 import string
 
-number = random.randint(0,100)
-letters = string.ascii_letters
-symbols = string.punctuation
-randomword = random.choice(letters)
+def randomStringGenerator(lengthOfString):
+    word = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation ) for n in range(lengthOfString)])
+    return word
 
-random = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation ) for n in range(12)])
-print(random)
+print(randomStringGenerator(100))
