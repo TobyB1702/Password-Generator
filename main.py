976cead89f3d13cfe65355c5e026685dd261d245
toby <toby.bell1702@hotmail.co.uk>
2020-07-17 12:04:17 +0100

    enabled to user to enable or disable upper, lower, number or punctation

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import tkinter as tk
 
 
 class Backend:
-    def randomStringGenerator(self, lengthOfString):
+    def randomStringGenerator(self, lengthOfString, bool1, bool2, bool3, boo4):
         b = Backend()
         word = ''.join(
-            [random.choice(b.parametersForString(True, True, True, True)) for n in range(lengthOfString)])
+            [random.choice(b.parametersForString(bool1, bool2, bool3, boo4)) for n in range(lengthOfString)])
         return word
 
-    def parametersForString(self, asciiLettersLower, asciiLettersUpper, digits, punctuation):
+    def parametersForString(self, asciiLettersUpper, asciiLettersLower, digits, punctuation):
         myWord = ""
         if asciiLettersLower:
             myWord += string.ascii_lowercase
@@ -37,24 +37,36 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.textBox = tk.Label(self, text="Enter Length of password").grid(row=0)
 
-        self.inputForLengthOfPassword = tk.Entry(self)
-        self.inputForLengthOfPassword.grid(row=0, column=1)
-        self.inputForLengthOfPassword.insert(0,"16")
+        self.inputForLengthOfPasswordE = tk.Entry(self)
+        self.inputForLengthOfPasswordE.grid(row=0, column=1)
+        self.inputForLengthOfPasswordE.insert(0, "16")
 
-        self.generate_Password = tk.Button(self, text="Generate a password",
-                                           command=lambda: self.get_password(int(self.inputForLengthOfPassword.get()))).grid(row=1)
+        uppercaseVar, lowercaseVar, digitsVar, punctuationVar = tk.BooleanVar(), tk.BooleanVar(), tk.BooleanVar(), tk.BooleanVar()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
+        self.uppercaseCB = tk.Checkbutton(self, text="Uppercase", variable=uppercaseVar, command=lambda: self.cb(uppercaseVar)).grid(row=3)
+        self.lowercaseCB = tk.Checkbutton(self, text="Lowercase", variable=lowercaseVar, command=lambda: self.cb(lowercaseVar)).grid(row=3, column=1)
+        self.digitsCB = tk.Checkbutton(self, text="Digits", variable=digitsVar, command=lambda: self.cb(digitsVar)).grid(row=3, column=2)
+        self.punctuationCB = tk.Checkbutton(self, text="Punctation", variable=punctuationVar, command=lambda: self.cb(punctuationVar)).grid(row=3, column=3)
+
+        self.generate_PasswordB = tk.Button(self, text="Generate a password",
+                                           command=lambda: self.get_password(
+                                               int(self.inputForLengthOfPasswordE.get()), uppercaseVar.get(), lowercaseVar.get(), digitsVar.get(), punctuationVar.get())).grid(row=1)
+
+        self.quitB = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy).grid(row=1, column=1)
 
-    def get_password(self, passwordLength):
+#refactor this
+    def get_password(self, passwordLength, bool1, bool2, bool3, boo4):
         getBackEnd = Backend()
-        test = getBackEnd.randomStringGenerator(passwordLength)
+        test = getBackEnd.randomStringGenerator(passwordLength, bool1, bool2, bool3, boo4)
         print(test)
 
     def setLengthOfPassword(self):
         size = 10
         print(size)
+
+    def cb(self,var):
+        print("variable is {0}".format(var.get()))
 
 
 root = tk.Tk()
