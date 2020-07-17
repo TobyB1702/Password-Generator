8ff7dd99e3191f3302e1e5ce21bf916058736c9d
toby <toby.bell1702@hotmail.co.uk>
2020-07-17 11:32:43 +0100

    Added in option to change size of password

--- main.py
+++ main.py
@@ -36,18 +36,25 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.textBox = tk.Label(self, text="Enter Length of password").grid(row=0)
-        self.inputForLengthOfPassword = tk.Entry(self).grid(row=0, column=1)
+
+        self.inputForLengthOfPassword = tk.Entry(self)
+        self.inputForLengthOfPassword.grid(row=0, column=1)
+        self.inputForLengthOfPassword.insert(0,"16")
 
         self.generate_Password = tk.Button(self, text="Generate a password",
-                                           command=self.get_password).grid(row=1)
+                                           command=lambda: self.get_password(int(self.inputForLengthOfPassword.get()))).grid(row=1)
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy).grid(row=1, column=1)
 
-    def get_password(self):
+    def get_password(self, passwordLength):
         getBackEnd = Backend()
-        test = getBackEnd.randomStringGenerator(100)
+        test = getBackEnd.randomStringGenerator(passwordLength)
         print(test)
+
+    def setLengthOfPassword(self):
+        size = 10
+        print(size)
 
 
 root = tk.Tk()
